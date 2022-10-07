2f740862f449a7d9f339210fe07b896d0b3f7c5f
YuyaAizawa <yaizawa@sa.cs.titech.ac.jp>
2022-10-08 01:13:39 +0900

    追加 固定文字

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html, div, input, text, button)
-import Html.Attributes exposing (class, value)
+import Html.Attributes exposing (class, value, size)
 import Html.Events exposing (onClick, onInput)
 import Random exposing (Generator)
 
@@ -27,7 +27,7 @@ type alias Model =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-  ( { format = "カニクリームコロッケ"
+  ( { format = "カ#ニク#リ#ー#ムコロ#ッケ"
     , result = ""
     }
   , Cmd.none
@@ -68,11 +68,42 @@ update msg ({ format, result } as model) =
 
 getSortee : String -> List Char
 getSortee format =
-  String.toList format
+  let
+    helper result rest =
+      case rest of
+        [] -> result
+
+        '#'::tl ->
+          case tl of
+            [] -> result
+            hd::tl_ -> helper result tl_
+
+        hd::tl -> helper (hd::result) tl
+  in
+    String.toList format
+      |> helper []
 
 construct : String -> List Char -> String
 construct format list =
-  String.fromList list
+  let
+    helper result formatRest listRest =
+      case ( formatRest, listRest ) of
+        ( [], _ ) ->
+          result
+
+        ( '#'::tl, _ ) ->
+          case tl of
+            [] -> result
+            hd::tl_ -> helper (hd::result) tl_ listRest
+
+        ( fhd::ftl, lhd::ltl ) ->
+          helper (lhd::result) ftl ltl
+
+        _ -> result
+  in
+    helper [] (String.toList format) list
+      |> List.reverse
+      |> String.fromList
 
 
 
@@ -82,7 +113,7 @@ view : Model -> Html Msg
 view { format, result } =
   div [ class "contents" ]
     [ div []
-        [ input [ onInput FormatUpdate, value format ] []
+        [ input [ onInput FormatUpdate, value format, size 40 ] []
         , button [ onClick Reroll ] [ text "生成" ]
         ]
     , div []
